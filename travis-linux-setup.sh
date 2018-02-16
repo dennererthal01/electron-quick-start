b5bc42d46e4f65432c10e22f78acd8bc97fbe780
dennererthal01 <dennererthal00@gmail.com>
2018-02-16 14:32:00 -0200

    test

---
+++ travis-linux-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+DOCKER_BUILDER_NAME='builder'
+THIS_PATH=$(dirname $0)
+
+if [ -n "$ARCH" ]; then DOCKER_IMAGE="$ARCH/$DOCKER_IMAGE"; fi
+
+docker run --name $DOCKER_BUILDER_NAME -e LANG=C.UTF-8 -e TERM \
+       -v $PWD:$PWD -w $PWD/$THIS_PATH -td $DOCKER_IMAGE
+
+docker exec -i $DOCKER_BUILDER_NAME apt-get update -q
+docker exec -i $DOCKER_BUILDER_NAME apt-get install -y snapcraft
+
+sudo apt-get -qq update
+sudo apt-get install wine
